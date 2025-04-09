15e0b7cba446b9a0c8db91ed073cbd6956629767
vandana-8081 <vandanavairy2004@gmail.com>
2025-04-09 11:07:44 +0530

    initial commite

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from courses.models import Course , Preriquisite ,Learning , Tag , Video
+
 
 # Register your models here.
+
+# class TagAdmin(admin.TabularInline):
+#     model = Tag
+
+# class LearnigAdmin(admin.TabularInline):
+#     model = Learning
+
+# class PreriquisiteAdmin(admin.TabularInline):
+#     model = Preriquisite  
+
+# class CourseAdmin(admin.ModelAdmin):
+#     inline = [TagAdmin ]
+
+admin.site.register(Course)
+admin.site.register(Tag)
+admin.site.register(Learning)
+admin.site.register(Preriquisite)
+admin.site.register(Video)
--- courses/models/__init__.py
+++ courses/models/__init__.py
@@ -1,1 +1,2 @@
 from courses.models.course import Course , Learning , Preriquisite , Tag
+from courses.models.video import Video
